7508e8fe0f05322dd78790d2e6e242d923409b0f
Eduardo Henrique Rocha <ehrocha@gmail.com>
2026-04-05 19:02:38 -0300

    v0.16.1: Bump version (versionCode 18)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ehrocha.pulsar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.16.0"
+        versionCode = 18
+        versionName = "0.16.1"
     }
 
     signingConfigs {
