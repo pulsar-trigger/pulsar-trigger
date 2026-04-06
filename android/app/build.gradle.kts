90cd037a4bedc8565eb503d5edb0375f104ca496
Eduardo Henrique Rocha <ehrocha@gmail.com>
2026-04-05 22:04:50 -0300

    v0.20.1: version bump

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ehrocha.pulsar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.20.0"
+        versionCode = 25
+        versionName = "0.20.1"
     }
 
     signingConfigs {
