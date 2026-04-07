6e2638cc54b86fb9fa1fa33caaff82c1dea14f62
Eduardo Henrique Rocha <ehrocha@gmail.com>
2026-04-06 22:45:59 -0300

    Bump Android version to 0.28.0 (versionCode 35)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ehrocha.pulsar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "0.27.0"
+        versionCode = 35
+        versionName = "0.28.0"
     }
 
     signingConfigs {
