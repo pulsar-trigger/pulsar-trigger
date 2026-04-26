5eb882bb4b3da4de7b6b0c46170b9f439dfcd457
Eduardo Henrique Rocha <ehrocha@gmail.com>
2026-04-26 01:13:00 -0300

    Wizard-first Intervalometer: use-case preset cards + collapsible Advanced (proving ground for direction C)
    Android v0.129.0 (code 140)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ehrocha.pulsar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 139
-        versionName = "0.128.0"
+        versionCode = 140
+        versionName = "0.129.0"
     }
 
     signingConfigs {
